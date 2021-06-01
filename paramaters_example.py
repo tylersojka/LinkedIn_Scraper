3d0cfbbf9cfd01ff48297bdf6e186b354430125c
tsojka <sojkasojkasojka@gmail.com>
2021-06-01 16:26:00 -0500

    added parameters example

---
+++ paramaters_example.py
@@ -0,0 +1,10 @@
+# search query 
+search_query = 'Conquest Cyber'
+
+
+# file where scraped profile information will be stored  
+file_name = 'results_file.csv'
+
+# login credentials
+linkedin_username = 'johndoe@gmail.com'
+linkedin_password = 'notpassw0rd'
